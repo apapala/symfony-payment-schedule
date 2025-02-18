fd1c8172284a677950ca20baf8e6242b0c243f6a
apapala <adam.papala@gmail.com>
2025-02-18 16:26:21 +0100

    Add monolog metrics channel and MetricsListener, add security bundle with default config

---
+++ src/src/EventListener/MetricsListener.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class MetricsListener implements EventSubscriberInterface
+{
+    private LoggerInterface $metricsLogger;
+
+    public function __construct(LoggerInterface $metricsLogger)
+    {
+        $this->metricsLogger = $metricsLogger;
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $event->getRequest()->attributes->set('_start_time', microtime(true));
+    }
+
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        if (!$startTime = $request->attributes->get('_start_time')) {
+            return;
+        }
+
+        $duration = microtime(true) - $startTime;
+
+        $metricsInfo = [
+            'duration' => $duration,
+            'status' => $event->getResponse()->getStatusCode(),
+            'method' => $request->getMethod(),
+            'path' => $request->getPathInfo()
+        ];
+
+        if ($route = $request->attributes->get('_route')) {
+            $metricsInfo['route'] = $route;
+        }
+
+        $this->metricsLogger->info('Request processed', $metricsInfo);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'kernel.request'  => ['onKernelRequest', 100],
+            'kernel.response' => ['onKernelResponse', -100],
+        ];
+    }
+}
